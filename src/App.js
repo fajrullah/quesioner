e8e2120ae69e359a98758c82648b0639ec167211
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2019-09-23 19:09:50 +0700

    [QUIZ]

--- src/App.js
+++ src/App.js
@@ -123,8 +123,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Quiz</h2>
+          <h2>Quiz Test</h2>
         </div>
         {this.state.result ? this.renderResult() : this.renderQuiz()}
       </div>
